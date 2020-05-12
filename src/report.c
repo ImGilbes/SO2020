3f799c6f7f8042edff9e868325e02f558be9311b
ImGilbes <64001410+ImGilbes@users.noreply.github.com>
2020-05-12 17:46:29 +0200

    report update 1

--- src/report.c
+++ src/report.c
@@ -10,28 +10,23 @@
 struct file_analysis *fileStructPointer(struct list *analysisList, char *fileName);
 
 int main(int argc, char **argv){
-    printf("benvenuto \n");
     bool doneFlag = false;
     char buff[100]; //100: limitazione superiore per la lunghezza di una riga
-    char *fname;
-    int charID;
-    int charDATA;
     struct list *fileList = list_new(); //lista contenente tutti i file_analysis* usati
     struct file_analysis *curFile; //puntatore alla struttura file da aggiornare e da cui prendere dati
     int* totChars;
-    int localsum;
-    int i,k;
+    int i,k, localsum, charID;
     struct list_iterator *iter;
+    unsigned long aggregated[128];
+    unsigned long aggrsum = 0;
 
     while(!doneFlag){ //ricevo input finchè non riceve comando di terminazione
         scanf("%s",buff);
         if(strcmp(buff,"done") != 0){ //done è il comando di fine input
             //leggo una linea, la parso, aggiungo i dati a quelli che già a avevo
-            fname = strtok(buff, delim);//leggo il file, se già è nella lista dei file ok, altrimenti devo creare una struttura file_analysis nuova per il file
-            curFile = fileStructPointer(fileList, fname);
+            curFile = fileStructPointer(fileList, strtok(buff, delim)); //prendo la file_analysis associata con il file anme di questa riga
             charID = atoi(strtok(NULL,delim));
-            charDATA = atoi(strtok(NULL,delim));
-            curFile->analysis[charID] = charDATA;//aggiungo i dati sul carattere nella struttura apposita
+            curFile->analysis[charID] = atoi(strtok(NULL,delim));//aggiungo i dati sul carattere nella struttura apposita*/
             //Passo alla prossima linea
         }
         else{
@@ -39,7 +34,7 @@ int main(int argc, char **argv){
         }
     }
 
-    //per ogni file calcolo il totale dei caratteri
+    //CICLO 1 : calcolo totale caretteri per ogni file e totale caratteri aggregato
     totChars = (int*)malloc(sizeof(int) * (fileList->lenght));
     iter = list_iterator_new(fileList);
     k=0;
@@ -47,9 +42,16 @@ int main(int argc, char **argv){
         localsum = 0;
         for(i = 0; i < 128; i++ ){
             localsum += curFile->analysis[i];
+            aggregated[i] = 0; //l'ho messo qui per non dover fare un'altro ciclo solo per inizializzarlo
         }
         totChars[k] = localsum;
+        aggrsum += localsum;
         k++;
+    }
+
+    //CILO 2: init aggregated sums for charactes
+    for(i = 0; i < 128; i++){
+        aggregated[i] = 0;
     }
 
     //Per ogni file_analysis struct creata, pusho in output le percentuali di presenza dei caratteri
@@ -60,11 +62,20 @@ int main(int argc, char **argv){
         printf("%s \n", curFile->file);
         for(i = 0; i < 128; i++ ){
             if(curFile->analysis[i] > 0){ //stampo i dati su un caratteri sse è stato trovato almeno una volta
-                printf("caratteri %c: %d (%f) \n", i, (int)curFile->analysis[i], ((float) curFile->analysis[i] / totChars[k]));
+                printf("caratteri %c: %ld (%f) \n", i, curFile->analysis[i], ((float) curFile->analysis[i] / totChars[k]));
+                aggregated[i] += curFile->analysis[i];
             }
         }
         k++;
     }
+
+    printf("\nAggregated data \n");
+    for( i = 0; i < 128; i++){
+        if(aggregated[i] > 0){
+            printf("caratteri %c: %ld (%f)\n", i, aggregated[i], (float)aggregated[i]/aggrsum);
+        }
+    }
+
     return 0;
 }
 
