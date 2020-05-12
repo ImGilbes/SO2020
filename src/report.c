b1dab10ae002ad0f44f8922dc1e977ebb5b11a68
ImGilbes <64001410+ImGilbes@users.noreply.github.com>
2020-05-12 17:07:32 +0200

    Add files via upload

---
+++ src/report.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "file_analysis.h"
+#include "list.h"
+
+#define delim ":"
+
+struct file_analysis *fileStructPointer(struct list *analysisList, char *fileName);
+
+int main(int argc, char **argv){
+    printf("benvenuto \n");
+    bool doneFlag = false;
+    char buff[100]; //100: limitazione superiore per la lunghezza di una riga
+    char *fname;
+    int charID;
+    int charDATA;
+    struct list *fileList = list_new(); //lista contenente tutti i file_analysis* usati
+    struct file_analysis *curFile; //puntatore alla struttura file da aggiornare e da cui prendere dati
+    int* totChars;
+    int localsum;
+    int i,k;
+    struct list_iterator *iter;
+
+    while(!doneFlag){ //ricevo input finchè non riceve comando di terminazione
+        scanf("%s",buff);
+        if(strcmp(buff,"done") != 0){ //done è il comando di fine input
+            //leggo una linea, la parso, aggiungo i dati a quelli che già a avevo
+            fname = strtok(buff, delim);//leggo il file, se già è nella lista dei file ok, altrimenti devo creare una struttura file_analysis nuova per il file
+            curFile = fileStructPointer(fileList, fname);
+            charID = atoi(strtok(NULL,delim));
+            charDATA = atoi(strtok(NULL,delim));
+            curFile->analysis[charID] = charDATA;//aggiungo i dati sul carattere nella struttura apposita
+            //Passo alla prossima linea
+        }
+        else{
+            doneFlag = true;
+        }
+    }
+
+    //per ogni file calcolo il totale dei caratteri
+    totChars = (int*)malloc(sizeof(int) * (fileList->lenght));
+    iter = list_iterator_new(fileList);
+    k=0;
+    while ((curFile = (struct file_analysis *)list_iterator_next(iter))){
+        localsum = 0;
+        for(i = 0; i < 128; i++ ){
+            localsum += curFile->analysis[i];
+        }
+        totChars[k] = localsum;
+        k++;
+    }
+
+    //Per ogni file_analysis struct creata, pusho in output le percentuali di presenza dei caratteri
+    //con il format specificato in README
+    iter = list_iterator_new(fileList);
+    k=0;
+    while ((curFile = (struct file_analysis *)list_iterator_next(iter))){
+        printf("%s \n", curFile->file);
+        for(i = 0; i < 128; i++ ){
+            if(curFile->analysis[i] > 0){ //stampo i dati su un caratteri sse è stato trovato almeno una volta
+                printf("caratteri %c: %d (%f) \n", i, (int)curFile->analysis[i], ((float) curFile->analysis[i] / totChars[k]));
+            }
+        }
+        k++;
+    }
+    return 0;
+}
+
+//returns the pointer to the file_analysis structure with fileName as file name
+//if such structure does not exist, it creates a new struct at the end of the list and returns
+//a pointer to that struct
+struct file_analysis *fileStructPointer(struct list *analysisList, char *fileName){
+    struct list_iterator *iter = list_iterator_new(analysisList);
+    struct file_analysis *curElement;
+
+    while ((curElement = (struct file_analysis *)list_iterator_next(iter))){
+        if(strcmp(curElement->file, fileName) == 0){
+            return curElement;
+        }
+    }
+
+    //creo una nuova struttura e la pusho in lista
+    curElement = file_analysis_new();
+    curElement->file = (char *)malloc(sizeof(char) * strlen(fileName)); //forse serve strlen+1 qui
+    strcpy(curElement->file, fileName);
+    list_push(analysisList, curElement);
+    return curElement;
+}
