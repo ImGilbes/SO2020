3545c460dc6c71b1ed9fda941d323f513361d337
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2020-06-07 10:38:58 +0200

    Aggiunto due file nuovi, corretto alcuni errori in fase di compilazione

---
+++ src/utilities.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <ctype.h>
+#include "bool.h"
+
+
+bool is_positive_number(char *s)
+{
+    if (s[0] == '-' || s[0] == '0')
+    {
+        return false;
+    }
+
+    int i;
+    for (i = 0; i < strlen(s); i++)
+    {
+        if (!isdigit(s[i]))
+            return false;
+    }
+    return true;
+}
---
+++ src/utilities.h
@@ -0,0 +1,6 @@
+#ifndef _UTILITIES_H_
+#define _UTILITIES_H_
+
+bool is_positive_number(char *s);
+
+#endif
